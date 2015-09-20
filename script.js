16aa24f1b36456b767988f852df2e1b9d644ce87
Ryan DeSha <rdesha0218@yahoo.com>
2015-09-20 00:20:19 -0700

    Works/crossout messed up

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
     $('button').click(function() {
     	var toAdd = $('input[name=list_item]').val();
-    	$('.list').append('<div class="item">' + toAdd + '</div>');
+    	$('.list').append('<div class="item">' + toAdd + '</div><button type="button" class="crossButton">Cross Out</button>');
     });
     $(document).on('click', '.item', function() {
     	$(this).remove();
     });
+
+    $(document).on('click', '.crossButton', function() {
+    	$('div.item').toggleClass('crossOut');
+    });
+/*
+    $(this).click(function(){
+    	$('.item').toggleClass('crossOut');
+    });
+*/
 });
 
 /*
+$('.list').append('<div class="item">' + toAdd + '</div><button type="button" class="crossOut">Cross Out</button>');
+    });
+
+
 $(document).ready(function() {
     $('#button').click(function() {
         var toAdd = $('input[name=checkListItem]').val();
@@ -28,4 +41,5 @@ $(document).ready(function() {
 		<br/>
 		<div class="list"></div>
 	</body>
+
 	*/
